095cc373836176e67b9244d001ce83ca227d0e41
pattysc <pattys@users.noreply.github.com>
2016-11-30 18:25:50 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  def initialize(findword)
+    @findword = findword
+  end
+  attr_reader :word
+
+  def match(array)
+    array.select do |word|
+      word.chars.sort == @findword.chars.sort
+    end
+  end
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+
+# => ["inlets"]
